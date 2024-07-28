d4dc4d124213cb85472a81b98d1eb4529d641f15
Leandro <leanfiadone@gmail.com>
2024-07-28 00:42:39 -0300

    correciones config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -46,7 +46,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
